a77a9e5ae75ec406dba33f0f7317d42da4cc8619
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2024-03-01 15:13:38 +0100

    init file

---
+++ backend/__init__.py
@@ -0,0 +1,35 @@
+"""Initializes the application"""
+import os
+from backend import config
+from flask import Flask
+from flask_cors import CORS
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+
+
+def create_app():
+    """"Creates an instance of the app"""
+
+    # Flask initialization and configuration setup
+    app = Flask(__name__)
+    environment_config = os.environ['ENVIRONMENT_CONFIGURATION']
+    app.config.from_object(environment_config)
+
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    # global strict slashes
+    app.url_map.strict_slashes = False
+
+    # Cross-Origin Resource Sharing
+    cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+    with app.app_context():
+        # Register blueprints
+        from backend.api.student import student_api_blueprint
+        app.register_blueprint(student_api_blueprint)
+        return app
