ef51c92a99ea92240b56025371425ccdc24acdd7
Engr Bassey <basseyleads001@gmail.com>
2024-03-21 13:17:37 +0100

    changes on req.txt

---
+++ dev-react/src/components/howitworks/howitworks.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const howitworks = () => {
+    return (
+        <div className='how-it-works'>
+            <div className="section-head">
+                <div className="head-contents">
+                    <h1>Apply to Become DevLine Mentor</h1>
+                    <p>Be a developers who is willing to share her knowledge with young developers.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default howitworks
